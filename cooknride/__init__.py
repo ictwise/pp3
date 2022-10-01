d2979c26ac3a646d450add8552035b6ed3164b93
ictwise <ictwise@gmail.com>
2022-10-01 11:21:24 +0000

    update jeroku's database_URL

--- cooknride/__init__.py
+++ cooknride/__init__.py
@@ -16,6 +16,10 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
--- cooknride/models.py
+++ cooknride/models.py
@@ -8,14 +8,14 @@ class Cuisine(db.Model):
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self.category_name
+        return self.cuisine_name
 
 
 class Users(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50), unique=True, nullable=False)
-    user_name = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
 
     def __repr__(self):
